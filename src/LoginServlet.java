58a963d78301d261aa82aa8caf0132b293110894
Mohammad Tanvir Islam <tanvieer@gmail.com>
2017-04-21 03:30:35 +0600

    FirstCommit

---
+++ src/LoginServlet.java
@@ -0,0 +1,34 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+
+        String userid = ((ServletRequest) response).getParameter("username");
+        String pass = ((ServletRequest) response).getParameter("password");
+      
+        out.println("Username: " + userid);  
+        out.println("<br/>Password: " + pass);        
+                
+	}
+
+}
