c17032bece144ec47d62e10e65a2ae38fbc5be2d
Mohammad Tanvir Islam <tanvieer@gmail.com>
2017-04-21 10:50:27 +0600

    APP Package

---
+++ src/app/LoginServlet.java
@@ -0,0 +1,33 @@
+package app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+
+        String userid = (String) request.getParameter("username");
+        String pass = (String) request.getParameter("password");
+      
+        out.println("Username: " + userid);  
+        out.println("Password: " + pass);        
+                
+	}
+
+}
---
+++ src/app/RegistrationServlet.java
@@ -0,0 +1,38 @@
+package app;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/RegistrationServlet")
+public class RegistrationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		String name = (String) request.getParameter("name");
+        String username = (String) request.getParameter("username");
+        String email = (String) request.getParameter("email");
+        String password = (String) request.getParameter("password");
+        String cpassword = (String) request.getParameter("cpassword");
+        
+        out.println("Name: " + name); 
+        out.println("Username: " + username);  
+        out.println("Email: " + email); 
+        out.println("Password: " + password);  
+        out.println("Confirm PassWord: " + cpassword); 
+	}
+
+}
